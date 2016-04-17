37f36081a2d794b00febeec5639d61d108344e40
Jonathan Arnett <jonarnett90@gmail.com>
2016-04-17 14:33:16 -0400

    Added stars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def stars(rating)
+    rating = rating.floor
+
+    num_stars = (rating / 2).to_i
+    star_string = "\uf005".encode('utf-8') * num_stars
+
+    if rating.odd?
+      star_string += "\uf089".encode('utf-8')
+    end
+
+    star_string
+  end
 end
