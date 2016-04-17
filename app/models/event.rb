1bcdc6b9c8728363808d4030aeb3de79b93c57a7
Jonathan Arnett <jonarnett90@gmail.com>
2016-04-17 02:51:59 -0400

    Basic model relationships

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ActiveRecord::Base
+  belongs_to :organization
+  has_many :reviews
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ActiveRecord::Base
+  belongs_to :event
+  has_many :votings
+
   def rating
     rating_fields = [:swag, :food, :environment, :prizes, :resources ]
     sum = rating_fields.reduce(0) { |acc, x| acc += self.send(x) }
