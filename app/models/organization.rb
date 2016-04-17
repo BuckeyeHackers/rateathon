0b7ad6f09e8cb1f69c22148832cad40c3b141416
Jonathan Arnett <jonarnett90@gmail.com>
2016-04-17 09:08:42 -0400

    Refactored org rating method

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,14 +3,12 @@ class Organization < ActiveRecord::Base
 
   def rating
     events = self.events.order(:date).to_a
-    denominator = (2 ** events.count) - 1
+    num_events = events.count
+    denominator = (2 ** num_events) - 1
 
-    sum = 0
-    while events.any?
-      numerator = (2 ** (events.count - 1)).to_f
-      event = events.delete_at(0)
-      sum += event.rating * (numerator / denominator)
+    events.each_with_index.reduce(0) do |acc, x|
+      numerator = (2 ** (num_events - x[1] - 1)).to_f
+      acc + x[0].rating * (numerator / denominator)
     end
-    sum
   end
 end
