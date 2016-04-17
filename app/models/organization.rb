cb00e3368255fad37294efea97e227049baeeb0c
Jonathan Arnett <jonarnett90@gmail.com>
2016-04-17 08:42:28 -0400

    Organization ratings

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,16 @@
 class Organization < ActiveRecord::Base
   has_many :events, dependent: :destroy
+
+  def rating
+    events = self.events.order(:date).to_a
+    denominator = (2 ** events.count) - 1
+
+    sum = 0
+    while events.any?
+      numerator = (2 ** (events.count - 1)).to_f
+      event = events.delete_at(0)
+      sum += event.rating * (numerator / denominator)
+    end
+    sum
+  end
 end
